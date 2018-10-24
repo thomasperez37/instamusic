3c123798ed969905b4c22793e93784c7907467eb
Louis Gabrie <louisgabrie@Louiss-MacBook-Air.local>
2018-10-24 18:20:31 -0400

    Resolved firebase conflict

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // Write your JS here
 import test from './script2.js';
 
+var database = firebase.database();
+var databaseRef = database.ref("/");
+
+databaseRef.once('value').then(function(snapshot) { 
+    // Use .val() to get the data from the snapshot
+    const databaseValues = snapshot.val();
+    console.log(databaseValues);
+});
+
 console.log('test');
 
 test();
