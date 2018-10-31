440a003584de50c4519bf0c17b77e714987e2cb3
thomasperez37 <thomasperez401@gmail.com>
2018-10-31 18:24:31 -0400

    fixed firebase and tested it

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 // Write your JS here
-import test from './script2.js';
+import {renderUser, userInfo} from './script2.js';
+
 
 var database = firebase.database();
 var databaseRef = database.ref("/");
 
+
+
+
+
 databaseRef.once('value').then(function(snapshot) { 
     // Use .val() to get the data from the snapshot
     const databaseValues = snapshot.val();
-    console.log(databaseValues);
+    renderUser(databaseValues.user1.name);
+    var keys = Object.keys(databaseValues.users)
+    userInfo(keys,databaseValues.users);
 });
-
-console.log('test');
-
-test();
--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,18 @@
-export default function test () {
-  console.log('test2')
-};
+export function userInfo(keys,data)
+{
+  for(var i = 0; i < keys.length; i++)
+  {
+    var key = keys[i];
+    var user = data[key];
+    console.log(key);
+    console.log(user.name);
+    console.log(user.email);
+    console.log(user.pass);
+    console.log('#########');
+  }
+}
+
+export function renderUser(data) 
+{
+  $("body").append(data);
+}
