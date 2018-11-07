a887fe46abf3642edc0e1911a7669e0f9fba3eea
Louis Gabrie <louisgabrie@Louiss-MacBook-Air.local>
2018-11-07 18:34:28 -0500

    Added test.js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 // Write your JS here
-import {} from './script2.js';
+import {renderUser, userInfo} from './script2.js';
 
+const database = firebase.database();
+const databaseRef = database.ref("/");
 
+databaseRef.once('value').then((snapshot) => { 
+    // "snapshot" is like the json of the database
+    const databaseValues = snapshot.val(); // Use .val() to get the data from the snapshot
+    renderUser(databaseValues.user1.name); // billy 
+
+    // userInfo function code
+    const keys = Object.keys(databaseValues.users) // points to the 'users' node in the database
+    userInfo(keys, databaseValues.users); 
+});
