cf1b6eae8233414fdd6f987a3b4f76ccbad93f36
thomasperez37 <thomasperez401@gmail.com>
2018-11-07 18:31:23 -0500

    Started making data call function

--- js/index.js
+++ js/index.js
@@ -1,18 +1,4 @@
 // Write your JS here
-import {renderUser, userInfo} from './script2.js';
+import {} from './script2.js';
 
 
-var database = firebase.database();
-var databaseRef = database.ref("/");
-
-
-
-
-
-databaseRef.once('value').then(function(snapshot) { 
-    // Use .val() to get the data from the snapshot
-    const databaseValues = snapshot.val();
-    renderUser(databaseValues.user1.name);
-    var keys = Object.keys(databaseValues.users)
-    userInfo(keys,databaseValues.users);
-});
--- js/script2.js
+++ js/script2.js
@@ -1,18 +1,12 @@
-export function userInfo(keys,data)
-{
-  for(var i = 0; i < keys.length; i++)
-  {
-    var key = keys[i];
-    var user = data[key];
-    console.log(key);
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.pass);
-    console.log('#########');
-  }
-}
+var database = firebase.database();
+var databaseRef = database.ref("/");
 
-export function renderUser(data) 
-{
-  $("body").append(data);
+
+
+function callWithSnapshot(callback) {
+  databaseRef.once('value').then(function(snapshot) { 
+    // Use .val() to get the data from the snapshot
+    var databaseValues = snapshot.val();
+    return callback(databaseValues);
+  });
 }
