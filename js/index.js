68bf5cb5d3df660ec30c15773b244d6eb957bd5a
thomasperez37 <thomasperez401@gmail.com>
2018-11-14 18:22:47 -0500

    Set up sign up function

--- js/index.js
+++ js/index.js
@@ -1,15 +1,2 @@
 // Write your JS here
-import {renderUser, userInfo} from './script2.js';
-
-const database = firebase.database();
-const databaseRef = database.ref("/");
-
-databaseRef.once('value').then((snapshot) => { 
-    // "snapshot" is like the json of the database
-    const databaseValues = snapshot.val(); // Use .val() to get the data from the snapshot
-    renderUser(databaseValues.user1.name); // billy 
-
-    // userInfo function code
-    const keys = Object.keys(databaseValues.users) // points to the 'users' node in the database
-    userInfo(keys, databaseValues.users); 
-});
+import {renderUser} from './script2.js';
