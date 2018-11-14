bea303f8867238b415678219e4aa409829d5bff0
Louis Gabrie <louisgabrie@Louiss-MacBook-Air.local>
2018-11-14 17:17:06 -0500

    Merge Conflicts

--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 var database = firebase.database();
 var databaseRef = database.ref("/");
 
@@ -10,41 +9,7 @@ function callWithSnapshot(callback) {
     var databaseValues = snapshot.val();
     return callback(databaseValues);
   });
-=======
-export function userInfo(keys, data) {
-  for(var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    var user = data[key];
-    console.log(key);
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.pass);
-    console.log('#########');
-  }
-}
 
 export function renderUser(data) { // this function just appends the data from databaseValues to the view
   $("body").append(data); // append data to the view
-} 
-
-
-
-
-const userInfo = (keys, data) => { 
-    for(let i = 0; i < keys.length; i++) { 
-        let key = keys[i]; 
-        let user = data[key]; 
-
-        console.log(key);
-        console.log(user.name);
-        console.log(user.email);
-        console.log(user.pass);
-        console.log('#########');
-    }
-}
-
-
-const renderUser = (data) => { // this function just appends the data from databaseValues to the view
-    $("body").append(data); // append data to the view 
->>>>>>> Stashed changes
-}
+};
--- js/test.js
+++ js/test.js
@@ -1,20 +1,20 @@
-// var firebaseRoot = "https://je-twitter-clone.firebase.com/twitterClone/", // points to the data in the database under a root branch
-//     usersRef = new Firebase(firebaseRoot + 'users'); // makes a reference to the 'users' node in the twitterClone database
+var firebaseRoot = "https://je-twitter-clone.firebase.com/twitterClone/", // points to the data in the database under a root branch
+    usersRef = new Firebase(firebaseRoot + 'users'); // makes a reference to the 'users' node in the twitterClone database
 
-// // this get's the value of the 'users' node
-// usersRef.on('value').then((snapshot) => {
-//     const databaseUsers = snapshot.val(); // gives you the json of the node in the database that you are referring to
-//     //  below this line, you do what you want with the data
-//     // ex: databaseUsers.user1.name = billy 
-// });
+// this get's the value of the 'users' node
+usersRef.on('value').then((snapshot) => {
+    const databaseUsers = snapshot.val(); // gives you the json of the node in the database that you are referring to
+    //  below this line, you do what you want with the data
+    // ex: databaseUsers.user1.name = billy 
+});
 
 
-// const database = firebase.database();
-// const databaseRef = database.ref("/");
-// // or 
-// const databaseRef = firebase.database().ref("/");
+const database = firebase.database();
+const databaseRef = database.ref("/");
+// or 
+const databaseRef = firebase.database().ref("/");
 
-// databaseRef.on("value").then((snapshot) => {
-//     const databaseValues = snapshot.val();
-
-// });
+databaseRef.on("value").then((snapshot) => {
+    const databaseValues = snapshot.val();
+    
+});
